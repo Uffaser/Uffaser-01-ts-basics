80ceadc63767226f923ced8cafcdd513fc4a6644
Uffaser <gerasimenkom405@gmail.com>
2025-12-26 13:35:11 +0200

    update

--- src/task-3.ts
+++ src/task-3.ts
@@ -1,11 +1,11 @@
-const usernames: string[] = ["alice", "bob", "charlie"];
-
-const ratings: number[] = [4.5, 3.8, 5];
-
 interface Product {
     id: number;
     title: string;
 }
+
+const usernames: string[] = ["alice", "bob", "charlie"];
+
+const ratings: number[] = [4.5, 3.8, 5];
 
 const products: Product[] = [
     { id: 1, title: "Phone" },
